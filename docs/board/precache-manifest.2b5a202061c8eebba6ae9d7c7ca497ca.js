dcebef08749eb997252268ed01a11a9f4a046a79
Daniel Baird <daniel@danielbaird.com>
2020-12-15 22:43:51 +1000

    reorg, still

--- docs/board/precache-manifest.2b5a202061c8eebba6ae9d7c7ca497ca.js
+++ docs/board/precache-manifest.2b5a202061c8eebba6ae9d7c7ca497ca.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3003339cf98221dc5300c965a468b472",
+    "revision": "57983f93a4181028d0984e9b7a692308",
     "url": "/bangbangboom/board/index.html"
   },
   {
-    "revision": "72d599e6239622a447b4",
+    "revision": "5f08a42fe490b532d703",
     "url": "/bangbangboom/board/static/css/main.c7b89340.chunk.css"
   },
   {
-    "revision": "f023b9094849580357eb",
-    "url": "/bangbangboom/board/static/js/2.b56dd1bb.chunk.js"
+    "revision": "09f8bef816ab6903a338",
+    "url": "/bangbangboom/board/static/js/2.c9ad58d3.chunk.js"
   },
   {
     "revision": "437e006683e70c85e9b13ecca02f15ea",
-    "url": "/bangbangboom/board/static/js/2.b56dd1bb.chunk.js.LICENSE.txt"
+    "url": "/bangbangboom/board/static/js/2.c9ad58d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "72d599e6239622a447b4",
-    "url": "/bangbangboom/board/static/js/main.02e52ee5.chunk.js"
+    "revision": "5f08a42fe490b532d703",
+    "url": "/bangbangboom/board/static/js/main.ec5b2b21.chunk.js"
   },
   {
     "revision": "4c5dcd649e1e401d0113",
--- docs/board/service-worker.js
+++ docs/board/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bangbangboom/board/precache-manifest.709de58e4a089566581c9ffd9bcb2b34.js"
+  "/bangbangboom/board/precache-manifest.2b5a202061c8eebba6ae9d7c7ca497ca.js"
 );
 
 self.addEventListener('message', (event) => {
